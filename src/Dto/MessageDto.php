3295223453175d35edf48b143635a3f7434213cb
Alexey Gromov <al.gromov@corp.mail.ru>
2019-06-06 23:52:28 +0300

    Updated error constants

--- src/Dto/MessageDto.php
+++ src/Dto/MessageDto.php
@@ -5,16 +5,7 @@ namespace SmsFeedback\Dto;
 
 class MessageDto
 {
-    public const STATUS_ACCEPTED                     = 'accepted';
-    public const STATUS_INVALID_MOBILE_PHONE         = 'invalid mobile phone';
-    public const STATUS_ERROR_AUTHORIZATION          = 'error authorization';
-    public const STATUS_TEXT_IS_EMPTY                = 'text is empty';
-    public const STATUS_TEXT_MUST_BE_STRING          = 'text must be string';
-    public const STATUS_SENDER_ADDRESS_INVALID       = 'sender address invalid';
-    public const STATUS_WAPURL_INVALID               = 'wapurl invalid';
-    public const STATUS_INVALID_SCHEDULE_TIME_FORMAT = 'invalid schedule time format';
-    public const STATUS_INVALID_STATUS_QUEUE_NAME    = 'invalid status queue name';
-    public const STATUS_NOT_ENOUGH_BALANCE           = 'not enough balance';
+    private const STATUS_ERROR = 'error';
 
     /** @var string */
     private $id;
@@ -36,5 +27,10 @@ class MessageDto
     public function getStatus(): string
     {
         return $this->status;
+    }
+
+    public function isError(): bool
+    {
+        return $this->status === self::STATUS_ERROR;
     }
 }
